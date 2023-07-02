f13b96d0f551e319ef32211cf2a8a3e899c2625b
Basil Victor <basilbennedict@gmail.com>
2023-07-02 21:13:54 +0530

    access request body

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,14 @@
 let router = require('express').Router();
 
 router.post('/login', (req, res, next) => {
+    let email = req.body.email;
+    let password = req.body.password;
     return res.status(200).json({
         status: 0,
+        data: {
+            email: email,
+            password: password
+        },
         message: "Login successful"
     });
 });
