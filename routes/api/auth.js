09c77d7f9081da0204f526cc5dd5f8b5b9dae5bc
Basil Victor <basilbennedict@gmail.com>
2023-07-02 21:27:50 +0530

    added validations

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 let router = require('express').Router();
+let {check, validationResult} = require('express-validator');
 
-router.post('/login', (req, res, next) => {
+router.post('/login', [check('email', 'Email is required and must be a valid email').notEmpty().isEmail(), check('password', 'Password is required').notEmpty()], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ status: -1, errors: errors.array(), message: 'Invaliid request' })
+    }
     let email = req.body.email;
     let password = req.body.password;
     return res.status(200).json({
